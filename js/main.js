586ae7fef598a71d3aa0ecd0975e9e3a25bdbb29
Michael Walters <18662201+yorkshiremoose@users.noreply.github.com>
2026-01-09 15:20:49 +0000

    Remove inline JS and add decent head tags

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 function firstAlert(message) {
     alert(message);
 }
+
+const button = document.getElementById('do-it');
+
+button.addEventListener('click', () => {
+    console.log('CLICK');
+    firstAlert("DONE");
+})
